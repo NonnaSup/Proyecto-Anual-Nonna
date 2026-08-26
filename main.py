550e014d80eedb7cb7cae5d8c497e4a10a818763
thiago.troncoso <thiago.troncoso@sagradoalcal.edu.ar>
2026-08-26 09:15:46 -0300

    se agrego el flask_cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 from modules.usuario import (
@@ -40,6 +41,7 @@ from modules.empleo import (
 )
 
 app = Flask(__name__)
+CORS(app) 
 
 # ----------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------
